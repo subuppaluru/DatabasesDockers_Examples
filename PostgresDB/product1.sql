e9dd7ed3d61b1a958622cb8cbd73164fa1b2fc51
subuppaluru <subuppaluru@gmail.com>
2022-11-19 22:26:25 +0530

    added postgres db

---
+++ PostgresDB/product1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE product (
+  product_id INT NOT NULL,
+  name varchar(250) NOT NULL,
+  price DECIMAL(5,0) NOT NULL,
+  PRIMARY KEY (product_id)
+);
+
+INSERT INTO product VALUES (1,'Xbox',100),(2,'PS4',400),(3,'iPhone',699);
+
+
